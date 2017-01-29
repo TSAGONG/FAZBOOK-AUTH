d055d0deb3d52ac02ff4b0939a6e37b364c3fe48
Tenzin Tsagong <tsagong@gmail.com>
2017-01-29 12:48:56 -0500

    add auth routes

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -6,3 +6,29 @@ const models = require('../db/models/index');
 function comparePass(userPassword, databasePassword) {
   return bcrypt.compareSync(userPassword, databasePassword);
 }
+
+// this will redirect a logged in user to their user profile
+// page if they're already loggin in.
+function loginRedirect(req, res, next) {
+  if(req.user) return res.status(401).json(
+    { status: 'You are already logged in' }
+  );
+    return next();
+}
+
+// allows users to register
+function createUser(req, res) {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(re.body.password, salt);
+
+  return models.User.create({
+    username: req.body.username,
+    password: hash,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    dob: req.body.dob
+  }).then(() => {
+    res.redirect('/');
+  });
+}
---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+// this adds the ability to register users.
+
+const express = require('express');
+const router = express.Router();
+
+const authHelpers = require('../auth/auth-helpers');
+const passport = require('../auth/local');
+
+router.get('./register', authHelpers.loginRedirect, (req, res) => {
+  res.render('auth/register');
+});
+
+router.post('/register', (req, res, next) => {
+  return authHelpers.createUser(req, res)
+  .then((response) => {
+    console.log('registration successful');
+  })
+  .catch((err) => { res.status(500).json({ status: 'error'}); });
+});
+
+// this route provides a page to log in
+router.get('/login', authHelpers.loginRedirect, (req, res) => {
+  res.render('auth/login');
+});
+
+//this checks
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/user',
+  failureRedirect: '/auth/login',
+  failureFlash: true
+  })
+);
+
+//this logsout the user and then redirects the user to the homepage
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
