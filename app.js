53a5a8b50b672430863c450159a26c272287584e
Tenzin Tsagong <tsagong@gmail.com>
2017-01-29 12:11:22 -0500

    add express-session, passport, new routes

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+const index = require('./routes/index');
+const authRoutes = require('./routes/auth.js');
+const userRoutes = require('./routes/user.js')
 // add new modules and files here
+
+// load environment variables
+require('dotenv').config();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,12 +26,23 @@ app.use(logger('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 // add new express-session and passport middleware here
+// tells express to use our express-session and passport middlewares
+app.user(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add route middleware here
-
+app.use('/', index);
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
